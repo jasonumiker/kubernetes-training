100901bfc684784b1dc7beb8faa0491b37517bff
Jason Umiker <jason.umiker@gmail.com>
2024-11-04 22:25:05 +1100

    Updated with version (to show Deployment)

--- probe-test-app/docker/build.sh
+++ probe-test-app/docker/build.sh
@@ -2,10 +2,16 @@
 docker buildx create --name mybuilder --bootstrap --use
 docker buildx build --push \
   --platform linux/arm64,linux/amd64 \
-  --tag jasonumiker/probe-test-app:031124 \
+  --tag jasonumiker/probe-test-app:latest \
   .
 docker buildx build --push \
   --platform linux/arm64,linux/amd64 \
-  --tag jasonumiker/probe-test-app:latest \
+  --tag jasonumiker/probe-test-app:v1 \
+  .
+sed -i -e 's/1/2/g' version.json
+docker buildx build --push \
+  --platform linux/arm64,linux/amd64 \
+  --tag jasonumiker/probe-test-app:v2 \
   .
 docker buildx rm mybuilder
+sed -i -e 's/2/1/g' version.json
--- probe-test-app/docker/probetest.py
+++ probe-test-app/docker/probetest.py
@@ -6,45 +6,50 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def return_status():
-    with open('probe-state.json', 'r') as file:
-        data = json.load(file)
-    file.close()    
+    with open('probe-state.json', 'r') as probe_state_file:
+        probe_state = json.load(probe_state_file)
+    probe_state_file.close()
+
+    with open('version.json', 'r') as version_file:
+        version = json.load(version_file)
+    version_file.close()
+
     if request.method == 'POST':
         if 'livez' in request.form:
-            if data["livez"] == 'Healthy':
-                data["livez"] = 'Unhealthy'
-                with open("probe-state.json", "w") as file:
-                    json.dump(data, file)
-                file.close()       
+            if probe_state["livez"] == 'Healthy':
+                probe_state["livez"] = 'Unhealthy'
+                with open("probe-state.json", "w") as probe_state_file:
+                    json.dump(probe_state, probe_state_file)
+                probe_state_file.close()       
                 return "Set Liveness Probe to Unhealthy"    
             else:
-                data["livez"] = 'Healthy'
-                with open("probe-state.json", "w") as file:
-                    json.dump(data, file)
-                file.close()
+                probe_state["livez"] = 'Healthy'
+                with open("probe-state.json", "w") as probe_state_file:
+                    json.dump(probe_state, probe_state_file)
+                probe_state_file.close()
                 return "Set Liveness Probe to Healthy"
         if 'readyz' in request.form:
-            if data["readyz"] == 'Healthy':
-                data["readyz"] = 'Unhealthy'
-                with open("probe-state.json", "w") as file:
-                    json.dump(data, file)
-                file.close()
+            if probe_state["readyz"] == 'Healthy':
+                probe_state["readyz"] = 'Unhealthy'
+                with open("probe-state.json", "w") as probe_state_file:
+                    json.dump(probe_state, probe_state_file)
+                probe_state_file.close()
                 return "Set Readiness Probe to Unhealthy"
             else:
-                data["readyz"] = 'Healthy'
-                with open("probe-state.json", "w") as file:
-                    json.dump(data, file)
-                file.close()
+                probe_state["readyz"] = 'Healthy'
+                with open("probe-state.json", "w") as probe_state_file:
+                    json.dump(probe_state, probe_state_file)
+                probe_state_file.close()
                 return "Set Readiness Probe to Healthy"
     elif request.method == 'GET':
-        return render_template('index.html', from_hostname=socket.gethostname(), readyz=data["readyz"], livez=data["livez"])
+        return render_template('index.html', from_hostname=socket.gethostname(), readyz=probe_state["readyz"], livez=probe_state["livez"], version=version["version"])
 
 @app.route("/livez")
 def return_livez():
-    with open('probe-state.json', 'r') as file:
-        data = json.load(file)
-    file.close() 
-    if data["livez"] == 'Healthy':
+    with open('probe-state.json', 'r') as probe_state_file:
+        probe_state = json.load(probe_state_file)
+    probe_state_file.close() 
+    if probe_state["livez"] == 'Healthy':
         return {
             "status": 200,
             "title": "OK"
@@ -57,10 +62,10 @@ def return_livez():
 
 @app.route("/readyz")
 def return_readyz():
-    with open('probe-state.json', 'r') as file:
-        data = json.load(file)
-    file.close() 
-    if data["readyz"] == 'Healthy':    
+    with open('probe-state.json', 'r') as probe_state_file:
+        probe_state = json.load(probe_state_file)
+    probe_state_file.close() 
+    if probe_state["readyz"] == 'Healthy':    
         return {
             "status": 200,
             "title": "OK"

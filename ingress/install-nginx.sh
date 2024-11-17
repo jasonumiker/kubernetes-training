433619b1b09b0bfc4d2a4724459115549c9a9de6
Jason Umiker <jumiker@atlassian.com>
2024-11-17 15:53:33 +1100

    Added Ingress example

--- ingress/install-nginx.sh
+++ ingress/install-nginx.sh
@@ -1,3 +1,3 @@
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo update
-helm install ingress ingress-nginx/ingress-nginx --version=4.11.3
+helm install ingress ingress-nginx/ingress-nginx --set controller.allowSnippetAnnotations=true --version=4.11.3
